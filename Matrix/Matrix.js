9cb55397a771e953a4fba489f6d4310a1191178c
RomaBureacov <RomanBureacov@gmail.com>
2026-01-16 02:56:05 -0800

    working matrix algebra with tests

---
+++ Matrix/Matrix.js
@@ -0,0 +1,190 @@
+/*
+This file represents basic matrix operations.
+
+Roman Bureacov
+ */
+
+/**
+ * Performs matrix multiplication, returning a new matrix.
+ *
+ * @param L the left matrix
+ * @param R the right matrix
+ * @return undefined if the multiplication is impossible, resulting matrix otherwise.
+ */
+const multiply = (L, R) => {
+    if (L.columns !== R.rows) return undefined;
+
+    const m = new Matrix(L.rows, R.columns)
+
+    for (let r = 0; r < L.rows; r++) {
+        for (let c = 0; c < R.columns; c++) {
+            let sum = 0;
+            for (let i = 0; i < L.columns; i++) {
+                sum += L.get(r, i) * R.get(i, c);
+            }
+            m.set(r, c, sum)
+        }
+    }
+
+    return m;
+}
+
+
+/**
+ * Finds the determinant of the matrix
+ * @param M the matrix
+ * @returns {number|null} null if the matrix is not square, the determinant otherwise
+ */
+const determinant = (M) => {
+    if (M.rows !== M.columns) return null;
+
+    if (M.rows === 1) {
+        return M.get(0, 0);
+    } else if (M.rows === 2) {
+        return M.get(0, 0) * M.get(1, 1) - M.get(0, 1) * M.get(1, 0);
+    } else {
+        let determinant = 0;
+        for (let c = 0; c < M.columns; c++) {
+            determinant += M.get(0, c) * cofactor(M, 0, c);
+        }
+        return determinant;
+    }
+}
+
+/**
+ * Finds the cofactor of the matrix
+ * @param M the matrix
+ * @param row the row index
+ * @param column the column index
+ * @returns {number|null} null if the matrix is non-square, the cofactor otherwise
+ */
+const cofactor = (M, row, column) => {
+    if (M.rows !== M.columns) return null;
+
+    let minor = new Matrix(M.rows - 1, M.columns - 1);
+    for (let r = 0, rMinor = 0; r < M.rows; r++) {
+        if (r !== row) {
+            for (let c = 0, cMinor = 0; c < M.columns; c++) {
+                if (c !== column) {
+                    minor.set(rMinor, cMinor, M.get(r, c));
+                    cMinor++;
+                }
+            }
+            rMinor++;
+        }
+    }
+
+    return Math.pow(-1, row + column) * determinant(minor);
+}
+
+/**
+ * Calculates the inverse of the matrix
+ * @param M
+ * @return null if the matrix is singular, the inverse of the matrix otherwise
+ */
+const inverse = (M) => {
+    if (M.rows !== M.columns) return null;
+
+    let dimension = M.rows;
+
+    // find the inverse using the adjoint
+    // A^-1 = 1/det(A) * adj(A) when det(A) != 0
+    // adj(A) returns the adjoint matrix is the transpose of the cofactor matrix of each term
+    const inverse = new Matrix(dimension);
+
+    for (let r = 0; r < dimension; r++) {
+        for (let c = 0; c < dimension; c++) {
+            inverse.set(r, c, cofactor(M, c, r));
+        }
+    }
+
+    let determinant = 0;
+    for (let c = 0; c < dimension; c++) {
+        determinant += M.get(0, c) * inverse.get(c, 0);
+    }
+
+    if (determinant === 0) return null;
+
+    inverse.matrix.forEach(e => e / determinant);
+
+    return inverse;
+}
+
+/**
+ * Class that represents a matrix.
+ *
+ * Matrix indices are 0-indexed.
+ *
+ * @author Roman Bureacov
+ */
+export class Matrix {
+    /**
+     * Creates a matrix of specified size with all entries 0
+     * @param rows the number of rows in the matrix
+     * @param columns the number of columns in the matrix
+     */
+    constructor(rows, columns = rows) {
+        Object.assign(this, {
+            rows, columns
+        });
+
+        this.matrix = []
+        for (let i = 0; i < rows * columns; i++) {
+            this.matrix[i] = 0;
+        }
+    }
+
+    /**
+     * Sets the value at the position.
+     *
+     * If the indices are out of bounds, nothing happens.
+     *
+     * @param row the row to look at
+     * @param col the column to look at
+     * @param value the value to set
+     */
+    set(row, col, value) {
+        if (this.matrix[row * this.columns + col] !== undefined)
+            this.matrix[row * this.columns + col] = value;
+    }
+
+    /**
+     * Gets the value at the position.
+     * @param row the row to look at
+     * @param col the column to look at
+     * @returns {*} the value at the point in the matrix
+     */
+    get(row, col) {
+        return this.matrix[row * this.columns + col];
+    }
+
+    /**
+     * Creates and returns an identity matrix.
+     * @param dimension the dimension of the matrix
+     * @returns {Matrix} the identity matrix
+     */
+    identity(dimension) {
+        let m = new Matrix(dimension);
+        for (let i = 0; i < dimension; i++) m.set(i, i, 1);
+        return m;
+    }
+
+    /**
+     * Duplicates and returns a copy of this matrix.
+     * @returns {Matrix}
+     */
+    duplicate() {
+        let m = new Matrix(this.rows, this.columns);
+        this.matrix.map( (v, i) => m.matrix[i] = v);
+        return m
+    }
+}
+
+
+/**
+ * Object that represents matrix operations.
+ *
+ * @author Roman Bureacov
+ * @type
+ */
+export const MatrixOp = { multiply, cofactor, inverse }
---
+++ test/MatrixTest.js
@@ -0,0 +1,200 @@
+import test from "node:test";
+import { strictEqual } from "node:assert/strict";
+import {Matrix, MatrixOp} from "../Matrix/Matrix.js";
+
+/**
+ * Fills a matrix with incrementing values 1-9
+ * @param M the matrix to fill
+ */
+const fillMatrix = (M) => {
+    for (let i = 0; i < M.matrix.length; i++) {
+        M.matrix[i] = (i % 9) + 1;
+    }
+}
+
+let approx = (A, B) => {
+    return (Math.abs(A - B) < 10e-6);
+};
+
+test("Make the identity matrix", () => {
+    let dimension = 4;
+
+    let actual = Matrix.prototype.identity(dimension);
+    for (let r = 0; r < dimension; r++) {
+        for (let c = 0; c < dimension; c++) {
+            if (r === c) {
+                strictEqual(actual.get(r, c), 1, `test at row ${r} and col ${c}`);
+            } else {
+                strictEqual(actual.get(r, c), 0, `test at row ${r} and col ${c}`);
+            }
+        }
+    }
+})
+
+test("Matrix creation", () => {
+
+    let m = new Matrix(2, 5);
+    let i = 0;
+    for (let r = 0; r < 2; r++) {
+        for (let c = 0; c < 5; c++) {
+            m.set(r, c, i++);
+        }
+    }
+
+    i = 0;
+    for (let r = 0; r < 2; r++) {
+        for (let c = 0; c < 5; c++) {
+            strictEqual(m.get(r, c), i++, `test at row ${r} and col ${c}`);
+        }
+    }
+})
+
+test("Matrix multiplication, 3x3 with a 3x1", () => {
+    let l = new Matrix(3, 3);
+    let r = new Matrix(3, 1);
+
+    fillMatrix(l)
+    fillMatrix(r)
+
+    let m = MatrixOp.multiply(l, r);
+
+    strictEqual(m.rows, 3);
+    strictEqual(m.columns, 1);
+
+    strictEqual(m.get(0, 0), 14);
+    strictEqual(m.get(1, 0), 32);
+    strictEqual(m.get(2, 0), 50);
+
+})
+
+test("Matrix multiplication, 4x4 with a 4x4", () => {
+    let l = new Matrix(4, 4);
+    let r = new Matrix(4, 4);
+
+    // fill 1-9
+    fillMatrix(l)
+
+    // fill 9-1
+    for (let i = 0, j = 8; i < r.matrix.length; i++, j--) {
+        r.matrix[i] = (() => {
+            if (j >= 0) return j + 1;
+            else return (j = 8) + 1;
+        })();
+    }
+
+    let m = MatrixOp.multiply(l, r);
+
+    strictEqual(m.rows, 4);
+    strictEqual(m.columns, 4);
+
+    // row 0
+    strictEqual(m.get(0, 0), 46);
+    strictEqual(m.get(0, 1), 63);
+    strictEqual(m.get(0, 2), 53);
+    strictEqual(m.get(0, 3), 43);
+    
+    // row 1
+    strictEqual(m.get(1, 0), 130);
+    strictEqual(m.get(1, 1), 167);
+    strictEqual(m.get(1, 2), 141);
+    strictEqual(m.get(1, 3), 115);
+    
+    // row 2
+    strictEqual(m.get(2, 0), 106);
+    strictEqual(m.get(2, 1), 109);
+    strictEqual(m.get(2, 2), 94);
+    strictEqual(m.get(2, 3), 79);
+    
+    // row 3
+    strictEqual(m.get(3, 0), 109);
+    strictEqual(m.get(3, 1), 141);
+    strictEqual(m.get(3, 2), 119);
+    strictEqual(m.get(3, 3), 97);
+})
+
+test("Matrix multiplication, failure with a 3x1 and a 3x3", () => {
+    let l = new Matrix(3, 1);
+    let r = new Matrix(3, 3);
+
+    fillMatrix(l);
+    fillMatrix(r);
+
+    strictEqual(MatrixOp.multiply(l, r), undefined);
+})
+
+test("Matrix inversion of a 3x3 matrix", () => {
+    let m = new Matrix(3);
+
+    m.set(0, 0, 5);
+    m.set(0, 1, 7);
+    m.set(0, 2, 4);
+
+    m.set(1, 0, 3);
+    m.set(1, 1, 5);
+    m.set(1, 2, 3);
+
+    m.set(2, 0, 5);
+    m.set(2, 1, 8);
+    m.set(2, 2, 5);
+    
+    let inverse = MatrixOp.inverse(m);
+
+    strictEqual(approx(inverse.get(0, 0), 1), true);
+    strictEqual(approx(inverse.get(0, 1), -3), true);
+    strictEqual(approx(inverse.get(0, 2), 1), true);
+    
+    strictEqual(approx(inverse.get(1, 0), 0), true);
+    strictEqual(approx(inverse.get(1, 1), 5), true);
+    strictEqual(approx(inverse.get(1, 2), -3), true);
+
+    strictEqual(approx(inverse.get(2, 0), -1), true);
+    strictEqual(approx(inverse.get(2, 1), -5), true);
+    strictEqual(approx(inverse.get(2, 2), 4), true);
+})
+
+test("Matrix inversion of a 4x4 matrix", () => {
+    let m = new Matrix(4);
+    
+    m.set(0, 0, 4);
+    m.set(0, 1, 9);
+    m.set(0, 2, 4);
+    m.set(0, 3, 2);
+
+    m.set(1, 0, 2);
+    m.set(1, 1, 5);
+    m.set(1, 2, 2);
+    m.set(1, 3, 1);
+
+    m.set(2, 0, 3);
+    m.set(2, 1, 9);
+    m.set(2, 2, 4);
+    m.set(2, 3, 1);
+
+    m.set(3, 0, 3);
+    m.set(3, 1, 7);
+    m.set(3, 2, 3);
+    m.set(3, 3, 2);
+    
+    let inverse = MatrixOp.inverse(m);
+
+    strictEqual(approx(inverse.get(0, 0), 2), true);
+    strictEqual(approx(inverse.get(0, 1), 1), true);
+    strictEqual(approx(inverse.get(0, 2), -1), true);
+    strictEqual(approx(inverse.get(0, 3), -2), true);
+
+    strictEqual(approx(inverse.get(1, 0), -1), true);
+    strictEqual(approx(inverse.get(1, 1), 2), true);
+    strictEqual(approx(inverse.get(1, 2), 0), true);
+    strictEqual(approx(inverse.get(1, 3), 0), true);
+
+    strictEqual(approx(inverse.get(2, 0), 1), true);
+    strictEqual(approx(inverse.get(2, 1), -5), true);
+    strictEqual(approx(inverse.get(2, 2), 1), true);
+    strictEqual(approx(inverse.get(2, 3), 1), true);
+
+    strictEqual(approx(inverse.get(3, 0), -1), true);
+    strictEqual(approx(inverse.get(3, 1), -1), true);
+    strictEqual(approx(inverse.get(3, 2), 0), true);
+    strictEqual(approx(inverse.get(3, 3), 2), true);
+    
+})
