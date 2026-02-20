9f3e9100083c38aaf8f934425b47c72f3f43dfe7
RomaBureacov <RomanBureacov@gmail.com>
2026-02-20 05:14:58 -0800

    made asset manager ignore already loaded assets

--- Game/assets/assetmanager.js
+++ Game/assets/assetmanager.js
@@ -77,7 +77,15 @@ export class AssetManager {
      */
     static async downloadOne(relativePath) {
         return new Promise((res, rej) => {
-
+            if (
+                this.imageCache[relativePath]
+                || this.textCache[relativePath]
+                || this.audioCache[relativePath]
+            ) { // this asset may or may not already exist
+                console.log(`Asset "${relativePath}" already loaded, skipping...`)
+                res(`Asset "${relativePath}" already loaded, skipping...`);
+            }
+            
             const absolutePath = this.#resolve(relativePath);
 
             let file;
